dfa6e5a3519ac930fd206c9e83fb355e888ec875
ANICK DELACOSTE <anicetkoue@gmail.com>
2022-05-24 07:19:28 +0000

    ajout nav bar video4 partie 2

---
+++ my_blog/blog/models.py
@@ -0,0 +1,14 @@
+from turtle import title
+from django.db import models
+
+# Create your models here.
+
+class Article(models.Model):
+    title = models.CharField(max_length=50)
+    description = models.TextField()
+    image  = models.FileField(upload_to="article_imgs")
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self) -> str:
+        return self.title
---
+++ my_blog/blog/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+from .models import Article
+# Create your views here.
+def home(request):
+    list_articles = Article.objects.all()
+    context ={"liste_articles": list_articles}
+    return render(request,'index.html', context)
